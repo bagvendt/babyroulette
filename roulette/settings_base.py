3d83a8c329ae87180728646ae9c4149ac1c07352
Marcus Gregersen <mbg@letsgo.dk>
2017-12-18 00:47:54 +0100

    Mere

--- roulette/settings_base.py
+++ roulette/settings_base.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'base.apps.BaseConfig',
-    'adminsortable2',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
