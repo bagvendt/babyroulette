a8ecefd2d3c676547facff7b620679c8df016d9f
Marcus Gregersen <mbg@letsgo.dk>
2018-01-31 21:20:44 +0100

    Settings marcus

--- roulette/settings_marcus.py
+++ roulette/settings_marcus.py
@@ -1,9 +1,25 @@
 from .settings_base import *
 try:
-    from .local import *
+    from .marcus_local import *
 except ImportError:
     pass
 
-#DEBUG = True
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 INSTALLED_APPS += ['marcus.apps.MarcusConfig']
+STARTING_CREDITS = 1000
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'marcus.sqlite3'),
+    }
+}
